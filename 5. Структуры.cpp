1e834a4e72b0482a01356ebfac64c4f83793ead7
Alex <alexxingplus@inbox.ru>
2018-12-17 10:34:04 +0300

    5 лаба переделана
    Переделал 5 лабу, теперь не выводит ошибку

--- 5. Структуры.cpp
+++ 5. Структуры.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 
-const string abc = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+const int a = 2;//количество записей в массиве
+
+const string abc = "1234567890АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
 
 int main (){
     int i, k, fh, fm;
@@ -13,27 +15,27 @@ int main (){
         int time;
         int fletter;
     };
-    train mas[7];
+    train mas[a];
     train free;
-    for (i=0; i<=7; i++){
+    for (i=0; i<a; i++){
         cout << "Введите название пункта назначения\n";
         cin >> mas[i].name;
-        cout << "Введите ночер поезда\n";
+        cout << "Введите номер поезда\n";
         cin >> mas[i].numb;
         cout << "Введите время отправления (Часы и минуты двумя числами через пробел)\n";
         cin >> mas[i].hour;
         cin >> mas[i].min;
         mas[i].time = mas[i].min + mas[i].hour*60;
     }
-    for (i=0; i<=7; i++){
-        for (k=0; k<33; k++){
+    for (i=0; i<a; i++){
+        for (k=0; k<43; k++){
             if (mas[i].name[0] == abc[k]){
                 mas[i].fletter = k;
             }
         }
     }
-    for (i=0; i<=7; i++){
-        for (k=0; k<7; k++){
+    for (i=0; i<a-1; i++){
+        for (k=0; k<a; k++){
             if (mas[i].fletter < mas[i+1].fletter){
                 free = mas[i];
                 mas[i] = mas[i+1];
@@ -41,10 +43,10 @@ int main (){
             }
         }
     }
-    cout << "Введите время (два числа через пробелы)";
+    cout << "Введите время (два числа через пробелы)\n";
     cin >> fh >> fm;
     fm = fm + fh*60;
-    for (i=0; i<=7; i++){
+    for (i=0; i<a; i++){
         if (mas[i].time > fm){
             cout << "\nНайден поезд, отправляющийся после заданного времени: \n";
             cout << "Место назначения: "<< mas[i].name << ";\n";
